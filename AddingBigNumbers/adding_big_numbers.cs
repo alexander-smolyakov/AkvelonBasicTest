267dc6d74815fceb2ecba091ad758684fbc184c5
Smolyakov Alexander <sphinx414@gmail.com>
2020-02-02 16:20:30 +0300

    Solution for task "Adding Big Numbers"

---
+++ AddingBigNumbers/adding_big_numbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace AddingBigNumbers
+{
+    class adding_big_numbers
+    {
+        public static string Add(string a, string b)
+        {
+            return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+        }
+
+        public static int Main()
+        {
+
+            string a = "123546512132132131313279876532";
+            string b = "454654879721321321321546578321";
+            string expected_result = "578201391853453452634826454853";
+
+            string result = Add(a, b);
+
+            Console.WriteLine(expected_result.SequenceEqual(result));
+
+            return 0;
+        }
+    }
+}
